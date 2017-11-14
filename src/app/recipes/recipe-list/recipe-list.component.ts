7e939168f4709946ac180ecbd53303516f33747a
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-14 19:56:22 +0530

    Managing Recipes in a RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -9,16 +11,19 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
+  recipes: Recipe[];
+
+ /*  recipes: Recipe[] = [
     new Recipe('Gobi Manchurian', 'This is a simple test', 
               'http://vegetarianindianrecipes.com/wp-content/uploads/2016/09/gobi-manchurian-recipe-8.jpg'),
     new Recipe('Another test recipe by Srivatsa', 'This is a simple test', 
               'http://vegetarianindianrecipes.com/wp-content/uploads/2016/09/gobi-manchurian-recipe-8.jpg')
-  ];
+  ]; */
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('Gobi Manchurian', 'This is a simple test', 
+                  'http://vegetarianindianrecipes.com/wp-content/uploads/2016/09/gobi-manchurian-recipe-8.jpg'),
+        new Recipe('Another test recipe by Srivatsa', 'This is a simple test', 
+                  'http://vegetarianindianrecipes.com/wp-content/uploads/2016/09/gobi-manchurian-recipe-8.jpg')
+      ];
+
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
 }
